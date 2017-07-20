34b4654355d311096e6f085a7dec9facf730e5b4
Dana Johnson <dnjohnson84@gmail.com>
2017-07-19 21:27:44 -0700

    Add US letter as paper size

--- lib/jekyll-pandoc-multiple-formats/printer.rb
+++ lib/jekyll-pandoc-multiple-formats/printer.rb
@@ -1,7 +1,7 @@
 # Copyright (c) 2012-2015 Nicolás Reynolds <fauno@endefensadelsl.org>
 #               2012-2013 Mauricio Pasquier Juan <mpj@endefensadelsl.org>
 #               2013      Brian Candler <b.candler@pobox.com>
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
@@ -9,10 +9,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -48,7 +48,8 @@ module JekyllPandocMultipleFormats
       a3paper:  32,
       a2paper:  64,
       a1paper: 128,
-      a0paper: 256
+      a0paper: 256,
+      letterpaper: 512
     }
 
     attr_accessor :output_file, :original_file, :pages, :template,
