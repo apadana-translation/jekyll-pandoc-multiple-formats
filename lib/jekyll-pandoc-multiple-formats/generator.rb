01023ef249b385731f01fe072542a574df596c90
Dana Johnson <dnjohnson84@gmail.com>
2018-04-07 14:32:59 -0700

    Build outputs only if docs have been modified since previous build
    Before building outputs for a doc, category, or collection, we compare the mtime of the raw .md files to the previous outputted files to see if the outputs actually need to be rebuilt. If not, we break out of the pandoc generation for that file. (.html files are still cleaned and built.)
    
    We still need to develop a method for checking mtime against modifications to other files that Pandoc uses (.tex files in _layout, config.yml, etc).

--- lib/jekyll-pandoc-multiple-formats/generator.rb
+++ lib/jekyll-pandoc-multiple-formats/generator.rb
@@ -1,7 +1,7 @@
 # Copyright (c) 2012-2018 Nicolás Reynolds <fauno@endefensadelsl.org>
 #               2012-2013 Mauricio Pasquier Juan <mpj@endefensadelsl.org>
 #               2013      Brian Candler <b.candler@pobox.com>
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
@@ -9,10 +9,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -51,12 +51,67 @@ class PandocGenerator < Generator
     @pandoc_files << pandoc_file
   end
 
+  def generate_collection_doc_for_output(collection, post, output)
+
+    # TODO get regex from collection data, not hard-coded
+    build_rel_path = post.relative_path.gsub(/_poems/, 'text')
+    build_rel_path = build_rel_path.gsub(/\.md\Z/, ".#{output}")
+    build_path = "#{@site.in_dest_dir.dup}/#{build_rel_path}"
+
+    # Does the output file already exist from a previous build?
+    if File.file?(build_path)
+
+      # Find out the modification times of the post and the existing output file
+      prev_build_mtime = File.mtime(build_path)
+      post_mtime = File.mtime(post.path)
+
+      # Don't build again if the post hasn't been modified since the previous build
+      if post_mtime < prev_build_mtime
+        Jekyll.logger.info "Pandoc:", "'#{post.data['title']}' has not been modified since the previous build. Skipping this file…"
+        # Add to keep_files so that old output file isn't cleaned
+        @site.keep_files << build_rel_path
+        return
+      end
+    end
+
+    pandoc_file = PandocFile.new(@site, output, post)
+    return unless pandoc_file.write
+    Jekyll.logger.info "Pandoc:", "Generating '#{post.data['title']}'"
+    @site.keep_files << pandoc_file.relative_path
+    @pandoc_files << pandoc_file
+  end
+
   def generate_category_for_output(category, docs, output)
-    # fetch corresponding category name from data file
+
+    # fetch corresponding category data
     categories_data = @site.data['categories']
     category_title = categories_data.dig(category, 'name')
+    cat_rel_path = @site.config.dig('pandoc', 'bundle_permalink').gsub(/:slug\.:output_ext/, "#{Utils.slugify(category_title)}.#{output}")
+    cat_path = "#{@site.in_dest_dir.dup}/#{cat_rel_path}"
 
-    Jekyll.logger.info 'Pandoc:', "Generating category #{category_title}"
+    # Does the output file already exist from a previous build?
+    if File.file?(cat_path)
+
+      # Get the mtime of the previous build
+      cat_prev_build_mtime = File.mtime(cat_path)
+      rebuild_cat = false
+
+      # Loop thru the docs to see if any of them have been modified since the category was previously built
+      docs.each do |doc|
+        if File.mtime(doc.path) > cat_prev_build_mtime
+          rebuild_cat = true
+          break
+        end
+      end
+
+      # Don't build again if there's not a recently modified doc
+      if rebuild_cat != true
+        Jekyll.logger.debug "Pandoc:", "Category '#{category_title}' has not been modified since the previous build. Skipping this file…"
+        # Add to keep_files so that old output file isn't cleaned
+        @site.keep_files << cat_rel_path
+        return
+      end
+    end
 
     sorted_docs = docs.sort_by { | doc |
       doc.data["order"] || 10000
@@ -71,14 +126,14 @@ class PandocGenerator < Generator
     end
 
     return unless pandoc_file.write
-
+    Jekyll.logger.info "Pandoc:", "Generating category '#{category_title}'"
     @site.keep_files << pandoc_file.relative_path
     @pandoc_files << pandoc_file
   end
 
   def generate_full_for_output(output)
     title = @site.config.dig('title')
-    Jekyll.logger.info 'Pandoc:', "Generating full file #{title}"
+    Jekyll.logger.info 'Pandoc:', "Generating full file '#{title}'"
     # We order collection by category, then by order.
     full = @site.posts.docs.reject { |p| p.data.dig('full') }.sort_by do |p|
       [ p.data['date'], p.data['categories'].first.to_s ]
@@ -91,9 +146,34 @@ class PandocGenerator < Generator
   end
 
   def generate_full_collection_for_output(collection, output)
-    # output entire collections of poems
     title = @site.config.dig('title')
-    Jekyll.logger.info 'Pandoc:', "Generating full collection file #{title}"
+
+    coll_rel_path = @site.config.dig('pandoc', 'bundle_permalink').gsub(/:slug\.:output_ext/, "#{Utils.slugify(title)}.#{output}")
+    coll_path = "#{@site.in_dest_dir.dup}/#{coll_rel_path}"
+
+    # Does the output file already exist from a previous build?
+    if File.file?(coll_path)
+
+      # Get the mtime of the previous build
+      coll_prev_build_mtime = File.mtime(coll_path)
+      rebuild_coll = false
+
+      # Loop thru the docs to see if any of them have been modified since the collection was previously built
+      collection.docs.each do |doc|
+        if File.mtime(doc.path) > coll_prev_build_mtime
+          rebuild_coll = true
+          break
+        end
+      end
+
+      # Don't build again if there's not a recently modified doc in the collection
+      if rebuild_coll != true
+        Jekyll.logger.info "Pandoc:", "Collection '#{title}' has not been modified since the previous build. Skipping this file…"
+        # Add to keep_files so that old output file isn't cleaned
+        @site.keep_files << coll_rel_path
+        return
+      end
+    end
 
     # sort by category (chapter), then frontmatter 'order' value
     full = collection.docs.sort_by do |doc|
@@ -102,6 +182,7 @@ class PandocGenerator < Generator
 
     full_file = PandocFile.new(@site, output, full, title, { full_collection: true })
     full_file.write
+    Jekyll.logger.info "Pandoc:", "Generating full collection '#{title}'"
     @site.keep_files << full_file.relative_path
     @pandoc_files << full_file
   end
@@ -116,7 +197,7 @@ class PandocGenerator < Generator
     @pandoc_files = []
 
     @config.outputs.each_pair do |output, _|
-      Jekyll.logger.info 'Pandoc:', "Generating #{output}"
+      Jekyll.logger.info 'Pandoc:', "Generating #{output} files"
       @site.posts.docs.each do |post|
         Jekyll::Hooks.trigger :posts, :pre_render, post, { format: output }
         generate_post_for_output(post, output) if @config.generate_posts?
@@ -126,7 +207,7 @@ class PandocGenerator < Generator
       @site.collections.each do |name, collection|
         collection.docs.each do |doc|
           Jekyll::Hooks.trigger :documents, :pre_render, doc, { format: output }
-          generate_post_for_output(doc, output) if @config.generate_posts?
+          generate_collection_doc_for_output(collection, doc, output) if @config.generate_posts?
           Jekyll::Hooks.trigger :documents, :post_render, doc, { format: output }
         end
       end
@@ -146,7 +227,7 @@ class PandocGenerator < Generator
 
       general_full_for_output(output) if @config.generate_full_file?
 
-      @site.collections.each do |name, collection|
+      @site.collections.reject { |c| c['posts'] }.each do |name, collection|
         generate_full_collection_for_output(collection, output) if @config.generate_full_collection_file?
       end
     end
